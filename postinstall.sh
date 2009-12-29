cd9293799e04705ec2e4db4d952f600cfc5df059
yselkowitz <yselkowitz@e127d73d-b18e-4ae1-a744-a22e6a3ccd30>
2009-12-29 20:56:23 +0000

    xorg/xinit: replace startxwin.{bat,sh} scripts with startxwin executable
    git-svn-id: https://cygwin-ports.svn.sourceforge.net/svnroot/cygwin-ports/ports/trunk@8394 e127d73d-b18e-4ae1-a744-a22e6a3ccd30

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
 /usr/bin/mkdir -p "$(/usr/bin/cygpath -AP)/Cygwin-X"
-/usr/bin/mkshortcut -AP -i /usr/bin/XWin.exe -n "Cygwin-X/XWin Server" -a /usr/bin/startxwin.bat /usr/bin/run.exe
+/usr/bin/mkshortcut -AP -i /usr/bin/XWin.exe -n "Cygwin-X/XWin Server" -a "/usr/bin/bash.exe -l -c /usr/bin/startxwin.exe" /usr/bin/run.exe
