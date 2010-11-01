0c41f2abd4e2996e6315d4da5f8462f51fb053db
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2010-10-31 22:07:39 -0500

    xorg/xinit 1.3.0

--- preremove.sh
+++ preremove.sh
@@ -1,2 +1,2 @@
-rm -f "$(cygpath -AP)/Cygwin-X/XWin Server.lnk"
-rmdir --ignore-fail-on-non-empty "$(cygpath -AP)/Cygwin-X"
+rm -f "$(cygpath $CYGWINFORALL -P)/Cygwin-X/XWin Server.lnk"
+rmdir --ignore-fail-on-non-empty "$(cygpath $CYGWINFORALL -P)/Cygwin-X"
