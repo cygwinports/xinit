0fab8cc3ca32def57143579b48338fcb036c2d79
Yaakov Selkowitz <yselkowi@redhat.com>
2015-01-12 15:45:28 -0600

    Avoid Start Menu collisions with dual-arch installations
    If a user installs both 32- and 64-bit X session packages on Cygwin
    x64, then the Start Menu shortcuts would collide.  Avoid this by
    renaming the Start Menu folder in the presumably non-default 32-on-64
    setup, which shouldn't affect most users.

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,3 @@
-/usr/bin/mkdir -p "$(/usr/bin/cygpath $CYGWINFORALL -P)/Cygwin-X"
-/usr/bin/mkshortcut $CYGWINFORALL -P -w / -i /usr/bin/XWin.exe -n "Cygwin-X/XWin Server" -a "--quote /usr/bin/bash.exe -l -c \"cd; /usr/bin/startxwin\"" /usr/bin/run.exe
+case $(uname -s) in *-WOW64) wow64=" (32-bit)" ;; esac
+/usr/bin/mkdir -p "$(/usr/bin/cygpath $CYGWINFORALL -P)/Cygwin-X${wow64}"
+/usr/bin/mkshortcut $CYGWINFORALL -P -w / -i /usr/bin/XWin.exe -n "Cygwin-X${wow64}/XWin Server" -a "--quote /usr/bin/bash.exe -l -c \"cd; /usr/bin/startxwin\"" /usr/bin/run.exe
--- preremove.sh
+++ preremove.sh
@@ -1,2 +1,3 @@
-rm -f "$(cygpath $CYGWINFORALL -P)/Cygwin-X/XWin Server.lnk"
-rmdir --ignore-fail-on-non-empty "$(cygpath $CYGWINFORALL -P)/Cygwin-X"
+case $(uname -s) in *-WOW64) wow64=" (32-bit)" ;; esac
+rm -f "$(cygpath $CYGWINFORALL -P)/Cygwin-X${wow64}/XWin Server.lnk"
+rmdir --ignore-fail-on-non-empty "$(cygpath $CYGWINFORALL -P)/Cygwin-X${wow64}"
