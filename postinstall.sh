75363676ba8658e5c928891a54b1eec4d38a86ec
Yaakov Selkowitz <yselkowi@redhat.com>
2015-02-08 02:38:14 -0600

    Fix WoW64 detection for cygwin-1.7.34

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,3 @@
-case $(uname -s) in *-WOW64) wow64=" (32-bit)" ;; esac
+case $(uname -s) in *-WOW*) wow64=" (32-bit)" ;; esac
 /usr/bin/mkdir -p "$(/usr/bin/cygpath $CYGWINFORALL -P)/Cygwin-X${wow64}"
 /usr/bin/mkshortcut $CYGWINFORALL -P -w / -i /usr/bin/XWin.exe -n "Cygwin-X${wow64}/XWin Server" -a "--quote /usr/bin/bash.exe -l -c \"cd; /usr/bin/startxwin\"" /usr/bin/run.exe
--- preremove.sh
+++ preremove.sh
@@ -1,3 +1,3 @@
-case $(uname -s) in *-WOW64) wow64=" (32-bit)" ;; esac
+case $(uname -s) in *-WOW*) wow64=" (32-bit)" ;; esac
 rm -f "$(cygpath $CYGWINFORALL -P)/Cygwin-X${wow64}/XWin Server.lnk"
 rmdir --ignore-fail-on-non-empty "$(cygpath $CYGWINFORALL -P)/Cygwin-X${wow64}"
